eea80b23d014310a2cc05c7c1eb0d92bd353d888
Eosiner <satria.riyan36@gmail.com>
2024-10-08 23:00:41 +0700

    8-10-2024, 22.53 Pada menu home masih prototype, masih perlu pembetulan lagi

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -52,6 +52,7 @@ class ShoeStoreHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
